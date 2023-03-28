a5ec09684ba1aaa55127e44a542aa3a33971b51e
an503 <126401391+an503@users.noreply.github.com>
2023-03-28 17:30:39 +0300

    Add files via upload

---
+++ chain/chain/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chain
+{
+    public class Train
+    {
+        private TrainWagon firstWagon;
+        private TrainWagon lastWagon;
+
+        private class TrainWagon
+        {
+            public int Number { get; set; }
+            public string Type { get; set; }
+            public TrainWagon PrevWagon { get; set; }
+            public TrainWagon NextWagon { get; set; }
+        }
+
+        public void AddWagon(int wagonCount, string wagonType)
+        {
+            for (int i = 0; i < wagonCount; i++)
+            {
+                var wagon = new TrainWagon()
+                {
+                    Number = GetWagonCount() + 1,
+                    Type = wagonType
+                };
+                if (lastWagon == null)
+                {
+                    firstWagon = wagon;
+                    lastWagon = wagon;
+                }
+                else
+                {
+                    lastWagon.NextWagon = wagon;
+                    wagon.PrevWagon = lastWagon;
+                    lastWagon = wagon;
+                }
+            }
+        }
+
+        public int GetWagonCount()
+        {
+            int count = 0;
+            var wagon = firstWagon;
+            while (wagon != null)
+            {
+                count++;
+                wagon = wagon.NextWagon;
+                //lastWagon.NextWagon = wagon;
+            }
+            return count;
+        }
+
+        public string GetAllWagons()
+        {
+            var s = new StringBuilder();
+            var wagon = firstWagon;
+            while (wagon != null)
+            {
+                s.AppendLine($"Вагон {wagon.Number} ({wagon.Type})");
+                wagon = wagon.NextWagon;
+            }
+            return s.ToString();
+        }
+    }
+
+
+
+}
---
+++ chain/chain/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var train = new Train();
+            train.AddWagon(5, "пасажирський");
+            train.AddWagon(3, "грузовий");
+            train.AddWagon(10, "Елітні");
+            textBox1.Text = train.GetAllWagons();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // var train = new Train();
+            //train.AddWagon(10, "Елітні");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
